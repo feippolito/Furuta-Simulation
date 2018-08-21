98aa5173373d264e067ef78583a7a96bdea71fce
Felipe Ippolito <feippolito@gmail.com>
2018-08-21 15:21:41 -0300

    observer

--- PenduloFuruta2/FurutaConstantsSymb.m
+++ PenduloFuruta2/FurutaConstantsSymb.m
@@ -24,7 +24,7 @@ delta = (M + .5*mp)*g*lp;
 %%Equations
 syms Phi dPhi Theta dTheta V;
 
-x0 = 0; %Linearisation around the point x0;
+x0 = pi; %Linearisation around the point x0;
 
 old = [Phi dPhi  Theta dTheta  V];
 new = [0     0    x0      0    0];
@@ -50,44 +50,44 @@ denddTheta=(alpha*beta)-(gamma*gamma)+((beta*beta+gamma*gamma)*sin(Theta)*sin(Th
 ddTheta = numddTheta/denddTheta;
 
 %% Linear model
-% 
-% A21 = diff(ddPhi,Phi);
-% A21 = vpa(subs(A21,old,new));
-% 
-% A22 = diff(ddPhi,dPhi);
-% A22 = vpa(subs(A22,old,new));
-% 
-% A23 = diff(ddPhi,Theta);
-% A23 = vpa(subs(A23,old,new));
-% 
-% A24 = diff(ddPhi,dTheta);
-% A24 = vpa(subs(A24,old,new));
-% 
-% A41 = diff(ddTheta,Phi);
-% A41 = vpa(subs(A41,old,new));
-% 
-% A42 = diff(ddTheta,dPhi);
-% A42 = vpa(subs(A42,old,new));
-% 
-% A43 = diff(ddTheta,Theta);
-% A43 = vpa(subs(A43,old,new));
-% 
-% A44 = diff(ddTheta,dTheta);
-% A44 = vpa(subs(A44,old,new));
-% 
-% A = double([ 0 1 0 0 ; A21 A22 A23 A24 ; 0 0 0 1; A41 A42 A43 A44]);
-% 
-% B2 = diff(ddPhi,V);
-% B2 = vpa(subs(B2,old,new));
-% 
-% B4 = diff(ddTheta,V);
-% B4 = vpa(subs(B4,old,new));
-% 
-% B = double([0 ;B2 ;0 ;B4]);
-% 
-% C = [0 0 1 0];
-% 
-% D = 0;
-% 
-% p = [(-2+j*2) (-2-j*2) -4 -1];
-% K = place(A,B,p);
+
+A21 = diff(ddPhi,Phi);
+A21 = vpa(subs(A21,old,new));
+
+A22 = diff(ddPhi,dPhi);
+A22 = vpa(subs(A22,old,new));
+
+A23 = diff(ddPhi,Theta);
+A23 = vpa(subs(A23,old,new));
+
+A24 = diff(ddPhi,dTheta);
+A24 = vpa(subs(A24,old,new));
+
+A41 = diff(ddTheta,Phi);
+A41 = vpa(subs(A41,old,new));
+
+A42 = diff(ddTheta,dPhi);
+A42 = vpa(subs(A42,old,new));
+
+A43 = diff(ddTheta,Theta);
+A43 = vpa(subs(A43,old,new));
+
+A44 = diff(ddTheta,dTheta);
+A44 = vpa(subs(A44,old,new));
+
+A = double([ 0 1 0 0 ; A21 A22 A23 A24 ; 0 0 0 1; A41 A42 A43 A44]);
+
+B2 = diff(ddPhi,V);
+B2 = vpa(subs(B2,old,new));
+
+B4 = diff(ddTheta,V);
+B4 = vpa(subs(B4,old,new));
+
+B = double([0 ;B2 ;0 ;B4]);
+
+C = [0 0 1 0];
+
+D = 0;
+
+p = [(-2+j*2) (-2-j*2) -4 -1];
+K = place(A,B,p);
--- PenduloFuruta2/Visualisation.m
+++ PenduloFuruta2/Visualisation.m
@@ -31,46 +31,113 @@ Xzaxis=[0;0];
 Yzaxis=[0;0];
 Zzaxis=[0;CAL];                 
 
-width  =  1;                    %Cube size
+width  =  1;                  
 length = 1;
 height = 1;
-L1=1
-P1=[-0.25  0.25  0.25];
-P2=[-0.25  0.25 -0.25];
-P3=[-0.25 -0.25  0.25];
-P4=[-0.25 -0.25 -0.25];
 
-P5=[L1 -0.25  0.25];
-P6=[L1 -0.25 -0.25];
-P7=[L1  0.25 -0.25];
-P8=[L1  0.25  0.25];
 
-X = [P1(1) P4(1) P1(1) P8(1)  P7(1) P6(1);
-     P2(1) P5(1) P2(1) P5(1)  P2(1) P5(1);
-     P3(1) P8(1) P7(1) P6(1)  P3(1) P4(1);
-     P4(1) P1(1) P8(1) P7(1)  P6(1) P3(1)];
+L1=2;
+W1=0.25;
 
-Y = [P1(2) P4(2) P1(2) P8(2)  P7(2) P6(2);
-     P2(2) P5(2) P2(2) P5(2)  P2(2) P5(2);
-     P3(2) P8(2) P7(2) P6(2)  P3(2) P4(2);
-     P4(2) P1(2) P8(2) P7(2)  P6(2) P3(2)];
+L2=1.5;
+W2=0.25;
 
-Z =[P1(3) P4(3) P1(3) P8(3)  P7(3) P6(3);
-     P2(3) P5(3) P2(3) P5(3)  P2(3) P5(3);
-     P3(3) P8(3) P7(3) P6(3)  P3(3) P4(3);
-     P4(3) P1(3) P8(3) P7(3)  P6(3) P3(3)];
+%Horizontal arm
 
+P1=[-W1  W1  W1];
+P2=[-W1  W1 -W1];
+P3=[-W1 -W1  W1];
+P4=[-W1 -W1 -W1];
+
+P5=[L1 -W1  W1];
+P6=[L1 -W1 -W1];
+P7=[L1  W1 -W1];
+P8=[L1  W1  W1];
+
+Xh=[P1(1) P3(1) P4(1) P2(1);
+    P2(1) P4(1) P6(1) P7(1);
+    P6(1) P7(1) P8(1) P5(1);
+    P7(1) P8(1) P1(1) P2(1);
+    P1(1) P8(1) P5(1) P3(1);
+    P3(1) P4(1) P6(1) P5(1)]';
+
+Yh=[P1(2) P3(2) P4(2) P2(2);
+    P2(2) P4(2) P6(2) P7(2);
+    P6(2) P7(2) P8(2) P5(2);
+    P7(2) P8(2) P1(2) P2(2);
+    P1(2) P8(2) P5(2) P3(2);
+    P3(2) P4(2) P6(2) P5(2)]';
+
+Zh=[P1(3) P3(3) P4(3) P2(3);
+    P2(3) P4(3) P6(3) P7(3);
+    P6(3) P7(3) P8(3) P5(3);
+    P7(3) P8(3) P1(3) P2(3);
+    P1(3) P8(3) P5(3) P3(3);
+    P3(3) P4(3) P6(3) P5(3)]';
+
+% Vertical arm
+
+p1=[L1     W2 W2];
+p2=[L1+2*W2  W2 W2];
+p3=[L1+2*W2 -W2 W2];
+p4=[L1    -W2 W2];
+
+p5=[L1     W2 -L2];
+p6=[L1+2*W2  W2 -L2];
+p7=[L1+2*W2 -W2 -L2];
+p8=[L1    -W2 -L2];
+
+Xv=[p1(1) p2(1) p3(1) p4(1);
+    p3(1) p4(1) p8(1) p7(1);
+    p8(1) p7(1) p6(1) p5(1);
+    p5(1) p8(1) p4(1) p1(1);
+    p1(1) p2(1) p6(1) p5(1);
+    p6(1) p7(1) p3(1) p2(1)]';
+
+Yv= [p1(2) p2(2) p3(2) p4(2);
+     p3(2) p4(2) p8(2) p7(2);
+     p8(2) p7(2) p6(2) p5(2);
+     p5(2) p8(2) p4(2) p1(2);
+     p1(2) p2(2) p6(2) p5(2);
+     p6(2) p7(2) p3(2) p2(2)]';
+
+Zv=  [p1(3) p2(3) p3(3) p4(3);
+      p3(3) p4(3) p8(3) p7(3);
+      p8(3) p7(3) p6(3) p5(3);
+      p5(3) p8(3) p4(3) p1(3);
+      p1(3) p2(3) p6(3) p5(3);
+      p6(3) p7(3) p3(3) p2(3)]';
+ 
 C = [10 ; 2 ; 4 ; 10 ; 6 ;10];
 
-X = X*length;
-Y = Y*width;
-Z = Z*height;
+Xh = Xh*length;
+Yh = Yh*width;
+Zh = Zh*height;
 
 map = [zColor;yColor;xColor;CubeColor];
 colormap(map);
 
 hold on
-cubo  = fill3(X,Y,Z,'b');
+Harm  = fill3(Xh,Yh,Zh,'b');           %Horizontal arm
+Varm  = fill3(Xv,Yv,Zv,'g');           %Vertical arm (Pendulum)
 axis([-AL AL -AL AL -AL AL]);
+
 grid on
 box on
+
+%% Update
+phi =0;
+for t=1:100*180
+    
+    
+
+         
+    set(Harm,'XData',Xh);
+    set(Harm,'YData',Yh);
+    set(Harm,'ZData',Zh);
+    set(Harm,'FaceVertexCData',C);
+    
+    
+    drawnow
+    pause(0.5); 
+end
